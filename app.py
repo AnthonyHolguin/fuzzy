4a73a692e6c86952fa95f65e6d243f6c2ee4cb65
AnthonyHolguin <aholguin@ec.krugercorp.com>
2022-11-27 15:31:05 -0500

    first commit

--- app.py
+++ app.py
@@ -393,4 +393,4 @@ class FuzzyData(Resource):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(host='0.0.0.0',debug=True, port=os.getenv("PORT", default=5000))
